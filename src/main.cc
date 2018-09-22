2b2d031075aa47ec3fbaf98313d37e7d97cadfc4
Ian Wakely <raveious.irw@gmail.com>
2018-09-22 10:31:47 -0400

    Updating how config data is fetched...
    ... and totally not removing my old key from the source. It's been changed...

--- src/main.cc
+++ src/main.cc
@@ -13,25 +13,21 @@ using namespace web;
 using namespace http;
 using namespace client;
 
-std::string get_configuration_data(std::string key) {
+template <typename T> T get_configuration_data(std::string key) {
     // Open up a Docker secret file for reading data from it
-    std::fstream fs;
-    fs.open ("/run/secrets/" + key, std::fstream::in);
+    std::fstream fs("/run/secrets/" + key, std::fstream::in);
+    T retVal;
+    char* temp = nullptr;
 
-    std::string retVal;
-
-    if (fs.is_open()){
+    if (fs.is_open()) {
+        // Read the contents of the file.
         fs >> retVal;
+    } else if ((temp = std::getenv(key.c_str())) != nullptr) {
+        // Try to see if we can find that value from the environment instead?
+        std::stringstream ss(temp);
+        ss >> retVal;
     } else {
-        // If that didn't work, look for an environment variable instead.
-        char* temp = nullptr;
-        if ((temp = std::getenv(key.c_str())) != nullptr) {
-            retVal = std::string(temp);
-        }
-    }
-
-    // Throw an exception if we couldn't find anything
-    if (retVal.empty()) {
+        // Throw an exception if we couldn't find anything
         throw std::runtime_error("Unable to find a value for " + key + ".");
     }
 
@@ -39,44 +35,49 @@ std::string get_configuration_data(std::string key) {
 }
 
 int main(int argc, char const *argv[]) {
+    uri_builder weather_uri(U("https://api.darksky.net/forecast/"));
 
     // Check for all of the things
     std::string key;
     try {
-        key = get_configuration_data("DARKSKY_KEY");
+        key = get_configuration_data<std::string>("DARKSKY_KEY");
     } catch (std::exception& e) {
         std::cerr << "Unable to determine DarkSky API key: " << e.what() << std::endl;
         return -1;
     }
 
-    std::string latitude;
+    weather_uri.append_path(key);
+
+    double latitude;
     try {
-        latitude = get_configuration_data("DARKSKY_LATITUDE");
+        latitude = get_configuration_data<double>("DARKSKY_LATITUDE");
     } catch (std::exception& e) {
         std::cerr << "Unable to determine latitude for DarkSky API: " << e.what() << std::endl;
         return -2;
     }
 
-    std::string longitude;
+    double longitude;
     try {
-        longitude = get_configuration_data("DARKSKY_LONGITUDE");
+        longitude = get_configuration_data<double>("DARKSKY_LONGITUDE");
     } catch (std::exception& e) {
         std::cerr << "Unable to determine longitude for DarkSky API: " << e.what() << std::endl;
         return -3;
     }
 
+    std::stringstream ss;
+    ss << latitude << ',' << longitude;
+    weather_uri.append(ss.str());
+
     std::chrono::milliseconds polling_rate;
     try {
-        polling_rate = std::chrono::milliseconds(std::stoi(get_configuration_data("DARKSKY_POLLING_RATE")));
+        polling_rate = std::chrono::milliseconds(get_configuration_data<int>("DARKSKY_POLLING_RATE"));
     } catch (std::exception& e) {
         std::cout << "Unable to determine DarkSky API polling rate, so we're just going to use 3000ms: " << e.what() << std::endl;
         polling_rate = std::chrono::milliseconds(3000);
     }
 
-    const uri weather_uri = uri_builder(U("https://api.darksky.net/forecast/98899c22b8f6057b063959ffd7086e4e/42.488769,-71.154438")).to_uri();
-
     // Creating client to make calls with
-    http_client client(weather_uri);
+    http_client client(weather_uri.to_uri());
 
     while (true) {
         client.request(methods::GET).then([](http_response response) -> pplx::task<json::value>
